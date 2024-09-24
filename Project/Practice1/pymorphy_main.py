07f264643d0ea66a3065011a0d0b1454181dbd13
Megarekrut65 <bandalak28@gmail.com>
2024-09-24 21:38:14 +0300

    Added examples for pymorphy3 and stanza

--- Project/Practice1/pymorphy_main.py
+++ Project/Practice1/pymorphy_main.py
@@ -3,8 +3,15 @@ import pymorphy3
 
 
 def main():
-    res = pymorphy3.analyzer.MorphAnalyzer(lang="uk").parse("грати")
-    print(res)
+    analyzer = pymorphy3.analyzer.MorphAnalyzer(lang="uk")
+    for i, word in enumerate(WORDS):
+        res = analyzer.parse(word)
+        for item in res:
+            forms = item.lexeme
+            print(f"\n{i + 1})\nСлово: {item.word}\nЛема: {item.normal_form}\n"
+                  f"Ознаки: {item.tag}\n\nФорми слова: ")
+            for form in forms:
+                print("\t", form.word)
 
 if __name__ == "__main__":
     main()
--- Project/Practice1/stanza_main.py
+++ Project/Practice1/stanza_main.py
@@ -3,10 +3,15 @@ import stanza
 
 
 def main():
-    stanza.download("uk")
+    #stanza.download("uk")
     nlp = stanza.Pipeline("uk")
-    res = nlp("грати")
-    print(res)
+    for i, word in enumerate(WORDS):
+        res = nlp(word)
+
+        for sen in res.sentences:
+            for word_ in sen.words:
+                print(f"{i+1})\nСлово: {word_.text}\nЛема: {word_.lemma}\nЧастина мови: {word_.pos}\n"
+                      f"Ознаки: {word_.feats}\n")
 
 if __name__ == "__main__":
     main()
